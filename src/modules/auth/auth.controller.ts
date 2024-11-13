04813b2e02bf1e29616931dedb7fe7516ba5cad8
Carlos Miguel <119144240+KarMiguel@users.noreply.github.com>
2024-11-13 08:15:40 -0300

    Adicionando swager

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,8 +2,10 @@
 import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Public } from './public.decorator'; 
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
+@ApiTags('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,10 +1,11 @@
-// user.controller.ts
 import { Controller, Get, NotFoundException, Query } from '@nestjs/common';
 import { CrudController } from '../../crud.controller';
 import { Usuario } from '@prisma/client';
 import { UserService } from './user.service';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('/user')
+@ApiTags('User')
 export class UserController extends CrudController<Usuario> {
   constructor(private readonly userService: UserService) {
     super(userService);
